b71642776c15a7e02d47a28f61e6395585cc1b08
cycycd <cycycd@qq.com>
2019-03-25 14:42:25 +0800

    fix git problem

--- src/main/java/com/cycycd/basictool/sort/Insertion.java
+++ src/main/java/com/cycycd/basictool/sort/Insertion.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public class Insertion extends Sort {
     public static <S extends Comparable<S>> void sort(S[] a) {
-        //to be write
+        //to be finished
     }
     public static <S extends Comparable<S>> void sort(List<S> a) {
-        //to be write
+        //to be finished
     }
 }
